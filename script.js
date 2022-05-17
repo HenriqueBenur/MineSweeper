dff51d7872b2f5fb4d7792d0316b04b6aa36fc31
HenriqueBenur <105389441+HenriqueBenur@users.noreply.github.com>
2022-05-17 18:13:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function start_game() {
         } break;
     }
 }
+var boardOn = false;
+var squares = []
 function game_easy() {
     let removeButton = document.getElementById("lastWill")
     removeButton.setAttribute("style","display: none")
@@ -19,8 +21,14 @@ function game_easy() {
     var height = 10
     var width = 8
     var bombAmount = 10
-    var squares = []
-    create_board(grid,height,width,squares,bombAmount)
+    let isGameOver = false
+    let flags = 0
+    
+    if (!boardOn) {
+        create_board(grid,height,width,squares,bombAmount,isGameOver,flags)
+    } else {
+        nanovisionEnabled(squares)
+    }
 }
 function game_normal() {
     let removeButton = document.getElementById("lastWill")
@@ -29,8 +37,13 @@ function game_normal() {
     var height = 18
     var width = 14
     var bombAmount = 40
-    var squares = []
-    create_board(grid,height,width,squares,bombAmount)
+    let isGameOver = false
+    let flags = 0
+    if (!boardOn) {
+        create_board(grid,height,width,squares,bombAmount,isGameOver,flags)
+    } else {
+        nanovisionEnabled(squares)
+    }
 }
 function game_hard() {
     let removeButton = document.getElementById("lastWill")
@@ -39,10 +52,15 @@ function game_hard() {
     var height = 24
     var width = 20
     var bombAmount = 99
-    var squares = []
-    create_board(grid,height,width,squares,bombAmount)
+    let isGameOver = false
+    let flags = 0
+    if (!boardOn) {
+        create_board(grid,height,width,squares,bombAmount,isGameOver,flags)
+    } else {
+        nanovisionEnabled(squares)
+    }
 }
-function create_board(grid,height,width,squares,bombAmount) {
+function create_board(grid,height,width,squares,bombAmount,isGameOver,flags) {
     const bombsArray = Array(bombAmount).fill('bomb')
     const safeArray = Array(height*width - bombAmount).fill('safe')
     const gameArray = safeArray.concat(bombsArray)
@@ -56,13 +74,17 @@ function create_board(grid,height,width,squares,bombAmount) {
         squares.push(square)
 
         square.addEventListener('click',function(e) {
-            click(square)
+            click(square, squares, width, isGameOver,height)
         })
+        square.oncontextmenu = function check(e) {
+            e.preventDefault()
+            addFlag(square,isGameOver,flags,bombAmount,squares)
+        }
     }
     for (let i = 0; i < squares.length; i++) {
         let total = 0
-        const isLeftEdge = i % width ===0
-        const isRightEdge = i === width -1
+        const isLeftEdge = (i % width ===0)
+        const isRightEdge = (i % width === width-1)
 
         if (squares[i].classList.contains('safe')) {
             if (i > 0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) {
@@ -90,13 +112,37 @@ function create_board(grid,height,width,squares,bombAmount) {
                 total++
             }
             squares[i].setAttribute('data',total)
-            console.log(squares[i])
+        }
+    }
+    boardOn = true
+}
+function addFlag(square,isGameOver, flags,bombAmount,squares) {
+    if (isGameOver) {
+        return
+    }
+    if (!square.classList.contains('checked') && (flags < bombAmount)) {
+        if (!square.classList.contains('flag')) {
+            square.classList.add('flag')
+            square.innerHTML = '🚩'
+            flags++
+            checkForWin(squares,bombAmount,isGameOver)
+        } else {
+            square.classList.remove('flag')
+            square.innerHTML = ''
+            flags--
         }
     }
 }
-function click(square) {
+function click(square, squares, width,isGameOver,height,bombAmount) {
+    let currentID = square.id
+    if (isGameOver) {
+        return
+    }
+    if (square.classList.contains('checked') || square.classList.contains('flag')) {
+        return
+    }
     if (square.classList.contains('bomb')) {
-        console.log('game over! tente novamente')
+        gameOver(square,isGameOver,squares)
     } else {
         let total = square.getAttribute('data')
         if (total != 0) {
@@ -104,6 +150,93 @@ function click(square) {
             square.innerHTML = total
             return
         }
-        square.classList.add('checked')
+        checkSquare(squares, width,isGameOver,height,currentID)
+    }
+    square.classList.add('checked')
+    checkForWin(squares,bombAmount,isGameOver)
+}
+function checkSquare(squares,width,isGameOver,height,currentID) {
+    
+    const isLeftEdge = currentID % width === 0
+    const isRightEdge = currentID % width === width-1
+
+    setTimeout(() => {
+        if (currentID > 0 && !isLeftEdge) {
+            const newID = squares[parseInt(currentID)-1].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID > (width-1) && !isRightEdge) {
+            const newID = squares[parseInt(currentID)+1-width].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID > width) {
+            const newID = squares[parseInt(currentID)-width].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID > (width+1) && !isLeftEdge) {
+            const newID = squares[parseInt(currentID)-1-width].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID < ((width*height)-1) && !isRightEdge) {
+            const newID = squares[parseInt(currentID)+1].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID < ((width*height)-width) && !isLeftEdge) {
+            const newID = squares[parseInt(currentID)-1+width].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID < ((width*height)-width-2) && !isRightEdge) {
+            const newID = squares[parseInt(currentID)+1+width].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+        if (currentID < ((width*height)-width-1) && !isLeftEdge) {
+            const newID = squares[parseInt(currentID)+width].id
+            const newSquare = document.getElementById(newID)
+            click(newSquare,squares,width,isGameOver)
+        }
+    }, 10)
+}
+function gameOver(square, isGameOver,squares) {
+    alert('Game Over, Você Perdeu')
+    isGameOver = true
+
+    squares.forEach(square => {
+        if (square.classList.contains('bomb')) {
+            square.innerHTML = '💣'
+        }
+    });
+}
+function checkForWin(squares,bombAmount,isGameOver) {
+    let BombMatch = 0;
+    let checkedSquares = 0;
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i].classList.contains('flag') && squares[i].classList.contains('bomb')) {
+            BombMatch++
+        }
+        if (squares[i].classList.contains('flag') && squares[i].classList.contains('cheat')) {
+            BombMatch++
+        }
+        if (squares[i].classList.contains('checked')) {
+            checkedSquares++
+        }
+        if (BombMatch === bombAmount && checkedSquares == (squares.length-bombAmount)) {
+            alert('ganhou o jogo rapaz')
+            isGameOver
+        }
+    }
+}
+function nanovisionEnabled(squares) {
+    for (let i = 0; i < squares.length; i++) {
+        if (squares[i].classList.contains('bomb')) {
+            squares[i].classList.remove('bomb')
+            squares[i].classList.add('cheat')
+        }
     }
 }
